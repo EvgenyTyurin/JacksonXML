d657421ffbe18b9c516d16cb3c45a781a36ce95f
EvgenyTyurin <beshere@yandex.ru>
2019-08-30 15:27:35 +0300

    init

---
+++ src/main/java/evgenyt/springdemo/App.java
@@ -0,0 +1,27 @@
+package evgenyt.springdemo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+/**
+ * Jackson FasterXML sample
+ * Aug 2019 EvgenyT
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+            ObjectMapper mapper = new XmlMapper();
+            // Person to XML
+            Person person1 = new Person();
+            person1.setName("Borden");
+            String xml = mapper.writeValueAsString(person1);
+            System.out.println("Person: " + person1.getName() + " -> XML: " + xml);
+            // XML to Person
+            Person person2 = mapper.readValue(xml, Person.class);
+            System.out.println("XML: " + xml + " -> Person: " + person2.getName());
+        } catch (Exception e) {e.printStackTrace();}
+    }
+}
---
+++ src/main/java/evgenyt/springdemo/Person.java
@@ -0,0 +1,23 @@
+package evgenyt.springdemo;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+/**
+ * Person <-> XML mapper
+ */
+
+@JacksonXmlRootElement(localName="person")
+public class Person {
+
+    @JacksonXmlProperty(localName = "name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
